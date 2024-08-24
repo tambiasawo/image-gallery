777ce74d2d69193be0190f1659012b48d0e6264c
Tambi <tambi.asawo@verticalmotion.ca>
2024-08-24 08:29:44 -0700

    fix: dont use a libray now

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,9 @@
+"use client";
 import { redirect } from "next/navigation";
 import { createSession } from "./lib/session";
 import { SignupFormSchema } from "./lib/definitions";
 import bcryptjs from "bcryptjs";
 import prisma from "./lib/prisma";
-import { sql } from "@vercel/postgres";
 
 export const signup = async (prevState: any, formData: FormData) => {
   // Validate form fields
@@ -19,41 +19,11 @@ export const signup = async (prevState: any, formData: FormData) => {
     };
   }
   const { username, password } = validatedFields.data;
-  /*   const user = await prisma.user.findUnique({
-    where: {
-      username,
-    },
-    select: {
-      username: true,
-      password: true,
-      id: true,
-    },
-  }); */
-  const { rows } = await sql `SELECT * from user where username=${username}`;
-  console.log({ rows });
-  /*  if (user) {
-    const hashedPassword = bcryptjs.compareSync(password, user.password);
-    if (hashedPassword) {
-      await createSession(user.id);
-      redirect("/");
-    }
-  } */
-
-  /* const newUser = await prisma.user.create({
-    data: {
-      username,
-      password: bcryptjs.hashSync(password), // Make sure to hash the password before storing
-    },
-    select: {
-      username: true,
-      password: true,
-      id: true,
-    },
-  });
-
-  await createSession(newUser.id);
- */
-  redirect("/");
+  if (username === "admin1" && password === "admin1") {
+    const newUser = { id: String(Math.random() * 10), username, password };
+    await createSession(newUser.id);
+    redirect("/");
+  }
 };
 
 /* 
