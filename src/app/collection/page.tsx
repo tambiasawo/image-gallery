16b0feab0563d1c5acdda7bf54df3be73b9b2546
Tambi <tambi.asawo@verticalmotion.ca>
2024-08-24 08:36:16 -0700

    feat: show liked and saved images

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,17 +1,51 @@
+"use client";
 import React from "react";
 import Divider from "@mui/material/Divider";
+import { useAppSelector } from "../store/hooks";
+import { getSavedImages } from "../store/savedSlice";
+import Image from "next/image";
 
 const Collection = () => {
+  const { saves: savedImages } = useAppSelector((state) => state.saves);
+  const { likes: likedImages } = useAppSelector((state) => state.likes);
+
+  const [displayedImages, setDisplayedImages] = React.useState(likedImages);
+  const [currentTab, setCurrentTab] = React.useState("likes");
+  const showLikedImages = () => {
+    setDisplayedImages(likedImages);
+    setCurrentTab("likes");
+  };
+
+  const showSavedImages = () => {
+    setDisplayedImages(savedImages);
+    setCurrentTab("saves");
+  };
+
+  console.log({ savedImages, likedImages, displayedImages });
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-4">
-        <div className="flex-auto h-[80vh]">
-          <ul className="flex flex-col gap-4">
-            <li>
-              <button className="collection-option ">Liked</button>
+        <div className="flex-auto">
+          <ul className="flex flex-row justify-center items-center md:items-start md:flex-col gap-4">
+            <li className="w-full">
+              <button
+                className={`collection-option ${
+                  currentTab === "likes" ? "bg-slate-200" : ""
+                }`}
+                onClick={showLikedImages}
+              >
+                Liked
+              </button>
             </li>
-            <li>
-              <button className="collection-option">Saved</button>
+            <li className="w-full">
+              <button
+                className={`collection-option ${
+                  currentTab === "saves" ? "bg-slate-200" : ""
+                }`}
+                onClick={showSavedImages}
+              >
+                Saved
+              </button>
             </li>
           </ul>
         </div>
@@ -21,7 +55,18 @@ const Collection = () => {
           flexItem
           className="hidden md:block"
         />
-        <div className="flex-grow flex-shrink basis-[70%]"></div>
+        <div className="flex-grow flex-shrink basis-[70%]">
+          <div className="flex flex-wrap gap-3 mt-8 items-center justify-center">
+            {displayedImages.map((image) => (
+              <img
+                key={image.img_id}
+                src={image.img_src}
+                alt="displayed image"
+                className="image-grid-item image-item"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
