7ae2dfe34074cbf3173a84a3fbb21566b1c62c08
Tambi <tambi.asawo@verticalmotion.ca>
2024-09-04 23:21:07 -0700

    fix:signin issue in production

--- src/app/_components/Gallery.tsx
+++ src/app/_components/Gallery.tsx
@@ -100,9 +100,6 @@ const Gallery = () => {
         <p> An error occurred. Please try again.</p>
       </div>
     );
-  React.useEffect(() => {
-    console.log({ session });
-  }, []);
 
   return (
     <div>
