44cddd3726bb6a822f87b1f0dc5f16fc7889b8aa
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-09 14:07:53 -0700

    fix: show loading on infinite scroll

--- src/app/_components/Gallery.tsx
+++ src/app/_components/Gallery.tsx
@@ -90,8 +90,6 @@ const Gallery = () => {
     const clientHeight = document.documentElement.clientHeight;
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    console.log({ clientHeight, scrollHeight, scrollTop });
-
     if (
       !isFetchingNextPage &&
       hasNextPage &&
@@ -108,7 +106,7 @@ const Gallery = () => {
     };
   }, [isFetchingNextPage, hasNextPage]);
 
-  if (isLoading)
+  /* if (isLoading)
     return (
       <div className="grid place-items-center h-[100vh]">
         <div className="w-[200px] h-[200px]">
@@ -116,14 +114,14 @@ const Gallery = () => {
         </div>
       </div>
     );
-
+ */
   if (error)
     return (
       <div className="flex justify-center text-red-500">
         <p> An error occurred. Please try again.</p>
       </div>
     );
-  if (images?.length === 0)
+  if (!isLoading && images?.length === 0)
     return (
       <div className="">
         <p className="text-red-500">No images found </p>
@@ -131,84 +129,87 @@ const Gallery = () => {
     );
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 justify-items-center gap-y-5">
-        {images.map((image: Image) => {
-          const { id, largeImageURL } = image;
-          const isSaved = savedImages.find((image) => image.img_id === id);
-          const isLiked = likedImages.find((image) => image.img_id === id);
+      {!isLoading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 justify-items-center gap-y-5">
+          {images.map((image: Image) => {
+            const { id, largeImageURL } = image;
+            const isSaved = savedImages.find((image) => image.img_id === id);
+            const isLiked = likedImages.find((image) => image.img_id === id);
 
-          return (
-            <div
-              onMouseEnter={(e) => handleMouseEnter(id)}
-              onMouseLeave={handleMouseLeave}
-              className="relative cursor-pointer image-container w-full h-[380px] max-w-[300px]"
-              key={id}
-            >
-              <img
-                className="w-full h-[380px] image-grid-item image-item object-cover"
-                src={largeImageURL}
-                alt={image.tags}
-                width={300}
-                height={300}
-                onClick={(e) => handleOpen(id)}
-              />
+            return (
+              <div
+                onMouseEnter={(e) => handleMouseEnter(id)}
+                onMouseLeave={handleMouseLeave}
+                className="relative cursor-pointer image-container w-full h-[380px] max-w-[300px]"
+                key={id}
+              >
+                <img
+                  className="w-full h-[380px] image-grid-item image-item object-cover"
+                  src={largeImageURL}
+                  alt={image.tags}
+                  width={300}
+                  height={300}
+                  onClick={(e) => handleOpen(id)}
+                />
 
-              {hoveredImageId === id && (
-                <div
-                  className={`
+                {hoveredImageId === id && (
+                  <div
+                    className={`
                 top-[45%] left-[35%] justify-center items-center absolute !brightness-150`}
-                >
-                  <p className="flex justify-center items-center gap-8 text-white ">
-                    <span className="flex flex-col items-center">
-                      {session ? (
-                        isLiked ? (
-                          <FavoriteIcon
-                            onClick={() => handleRemoveLike(id, largeImageURL)}
-                            className="text-red-600"
-                          />
+                  >
+                    <p className="flex justify-center items-center gap-8 text-white ">
+                      <span className="flex flex-col items-center">
+                        {session ? (
+                          isLiked ? (
+                            <FavoriteIcon
+                              onClick={() =>
+                                handleRemoveLike(id, largeImageURL)
+                              }
+                              className="text-red-600"
+                            />
+                          ) : (
+                            <FavoriteBorderIcon
+                              onClick={() => handleAddLike(id, largeImageURL)}
+                            />
+                          )
                         ) : (
-                          <FavoriteBorderIcon
-                            onClick={() => handleAddLike(id, largeImageURL)}
-                          />
-                        )
-                      ) : (
-                        ""
-                      )}
-                    </span>
+                          ""
+                        )}
+                      </span>
 
-                    <span className="flex flex-col items-center">
-                      {session ? (
-                        isSaved ? (
-                          <BookmarkIcon
-                            onClick={() =>
-                              handleRemoveBookmark(id, largeImageURL)
-                            }
-                          />
+                      <span className="flex flex-col items-center">
+                        {session ? (
+                          isSaved ? (
+                            <BookmarkIcon
+                              onClick={() =>
+                                handleRemoveBookmark(id, largeImageURL)
+                              }
+                            />
+                          ) : (
+                            <BookmarkBorderOutlinedIcon
+                              onClick={() =>
+                                handleAddBookmark(id, largeImageURL)
+                              }
+                            />
+                          )
                         ) : (
-                          <BookmarkBorderOutlinedIcon
-                            onClick={() => handleAddBookmark(id, largeImageURL)}
-                          />
-                        )
-                      ) : (
-                        ""
-                      )}
-                    </span>
-                  </p>
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="flex justify-center my-8">
-        <Pagination
-          count={Math.floor(count / ITEMS_PER_PAGE)}
-          color="primary"
-          onChange={handleChange}
-          page={page}
-        />
-      </div>
+                          ""
+                        )}
+                      </span>
+                    </p>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="grid place-items-center h-[100vh]">
+          <div className="w-[200px] h-[200px]">
+            <CircularProgress />
+          </div>
+        </div>
+      )}
       <LightBoxModal
         open={open}
         onClose={handleClose}
